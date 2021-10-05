9b51411a630e75507a109d19ad1abd2dbd8448c1
Dariusz Kozłowski <dariusz.kozlowski1997@gmail.com>
2021-10-05 21:22:52 +0200

    Files renaming

--- src/main/java/presentation/Menu.java
+++ src/main/java/presentation/Menu.java
@@ -11,7 +11,7 @@ public class Menu {
 
     public void mainMenu() throws SQLException, IOException {
        String[] options = {"Clubs", "Coaches", "Matches", "Players", "Referees", "Statistics", "Exit"};
-       int result = JOptionPane.showOptionDialog(null, "What do you need to check?", "Main menu",
+       int result = JOptionPane.showOptionDialog(null, "What do you need to check?", "Ekstraklasa Database",
                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[options.length-1]);
        switch (result) {
            case 0 -> {
@@ -42,7 +42,7 @@ public class Menu {
     private void clubMenu() throws SQLException, IOException {
         ClubOption clubOption = new ClubOption();
         String[] clubOptions = {"Display all", "Info about club", "Display club players", "Display club coach", "Display club results", "Cancel"};
-        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Club menu",
+        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Ekstraklasa Database (Club)",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, clubOptions, clubOptions[clubOptions.length-1]);
         switch (result) {
             case 0 -> {
@@ -50,28 +50,28 @@ public class Menu {
                 clubMenu();
             }
             case 1 -> {
-                String club = JOptionPane.showInputDialog("Enter ClubID or name");
+                String club = JOptionPane.showInputDialog("Enter Club ID or name");
                 if(club != null) {
                     clubOption.displaySpecificOne(club);
                 }
                 clubMenu();
             }
             case 2 -> {
-                String club = JOptionPane.showInputDialog("Entry ClubID or name");
+                String club = JOptionPane.showInputDialog("Enter Club ID or name");
                 if(club != null) {
                     clubOption.displayClubPlayers(club);
                 }
                 clubMenu();
             }
             case 3 -> {
-                String club = JOptionPane.showInputDialog("Entry ClubID or name");
+                String club = JOptionPane.showInputDialog("Enter Club ID or name");
                 if(club != null) {
                     clubOption.displayClubCoach(club);
                 }
                 clubMenu();
             }
             case 4 -> {
-                String club = JOptionPane.showInputDialog("Entry ClubID or name");
+                String club = JOptionPane.showInputDialog("Enter Club ID or name");
                 if(club != null) {
                     clubOption.displayClubResults(club);
                 }
@@ -86,7 +86,7 @@ public class Menu {
     private void coachMenu() throws SQLException, IOException {
         CoachOption coachOption = new CoachOption();
         String[] coachOptions = {"Display all", "Info about coach", "Cancel"};
-        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Coach menu",
+        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Ekstraklasa Database (Coach)",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, coachOptions, coachOptions[coachOptions.length-1]);
         switch (result) {
             case 0 -> {
@@ -94,7 +94,7 @@ public class Menu {
                 coachMenu();
             }
             case 1 -> {
-                String coach = JOptionPane.showInputDialog("Enter one of them: CoachID, ClubID  or Coach last name");
+                String coach = JOptionPane.showInputDialog("Enter one of them: Coach ID, Club ID  or Coach last name");
                 if(coach != null) {
                     int value;
                     try {
@@ -115,7 +115,7 @@ public class Menu {
     private void matchMenu() throws SQLException, IOException {
         MatchOption matchOption = new MatchOption();
         String[] matchOptions = {"Display all", "Info about match", "Gameweek results", "Matches by referee", "Club results", "Cancel"};
-        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Match menu",
+        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Ekstraklasa Database (Match)",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, matchOptions, matchOptions[matchOptions.length-1]);
         switch (result) {
             case 0 -> {
@@ -123,7 +123,7 @@ public class Menu {
                 matchMenu();
             }
             case 1 -> {
-                String match = JOptionPane.showInputDialog("Enter MatchID");
+                String match = JOptionPane.showInputDialog("Enter Match ID");
                 if(match != null)  {
                     matchOption.displaySpecificOne(match);
                 }
@@ -142,7 +142,7 @@ public class Menu {
                 matchMenu();
             }
             case 3 -> {
-                String match = JOptionPane.showInputDialog("Entry referee id or last name");
+                String match = JOptionPane.showInputDialog("Enter referee ID or last name");
                 if(match != null) {
                     try {
                         int value = Integer.parseInt(match);
@@ -154,7 +154,7 @@ public class Menu {
                 matchMenu();
             }
             case 4 -> {
-                String match = JOptionPane.showInputDialog("Entry club id or name");
+                String match = JOptionPane.showInputDialog("Entry club ID or name");
                 if(match != null) {
                     matchOption.displayByClub(match);
                 }
@@ -169,7 +169,7 @@ public class Menu {
     private void playerMenu() throws SQLException, IOException {
         PlayerOption playerOption = new PlayerOption();
         String[] playerOptions = {"Display all", "Info about player", "Players by position", "Cancel"};
-        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Match menu",
+        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Ekstraklasa Database (Player)",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, playerOptions, playerOptions[playerOptions.length - 1]);
         switch (result) {
             case 0 -> {
@@ -177,14 +177,14 @@ public class Menu {
                 playerMenu();
             }
             case 1 -> {
-                String player = JOptionPane.showInputDialog("Entry player id or last name");
+                String player = JOptionPane.showInputDialog("Enter player ID or last name");
                 if(player != null) {
                     playerOption.displaySpecificOne(player);
                 }
                 playerMenu();
             }
             case 2 -> {
-                String player = JOptionPane.showInputDialog("Entry position (for example: \"Goalkeeper\" ");
+                String player = JOptionPane.showInputDialog("Enter position (for example: \"Goalkeeper\" ");
                 if(player != null) {
                     playerOption.displaySamePositionPlayers(player);
                 }
@@ -199,7 +199,7 @@ public class Menu {
     private void refereeMenu() throws SQLException, IOException {
         RefereeOption refereeOption = new RefereeOption();
         String[] refereeOptions = {"Display all", "Info about referee", "Cancel"};
-        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Referee menu",
+        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Ekstraklasa Database (Referee)",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, refereeOptions, refereeOptions[refereeOptions.length-1]);
         switch (result) {
             case 0 -> {
@@ -207,7 +207,7 @@ public class Menu {
                 refereeMenu();
             }
             case 1 -> {
-                String referee = JOptionPane.showInputDialog("Entry referee id or last name");
+                String referee = JOptionPane.showInputDialog("Enter referee ID or last name");
                 if(referee != null) {
                     try {
                         int value = Integer.parseInt(referee);
@@ -227,7 +227,7 @@ public class Menu {
     private void statisticMenu() throws SQLException, IOException {
         StatisticOption statisticOption = new StatisticOption();
         String[] statisticOptions = {"Display all", "Info about statistic", "Statistics by Player", "Statistics by Match", "Cancel"};
-        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Statistic menu",
+        int result = JOptionPane.showOptionDialog(null, "Choose the right option", "Ekstraklasa Database (Statistic)",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, statisticOptions, statisticOptions[statisticOptions.length-1]);
         switch (result) {
             case 0 -> {
@@ -235,21 +235,21 @@ public class Menu {
                 statisticMenu();
             }
             case 1 -> {
-                String statistic = JOptionPane.showInputDialog("Enter statistic id (for example: \"LGD-RAD:LGD#01\")");
+                String statistic = JOptionPane.showInputDialog("Enter statistic ID (for example: \"LGD-RAD:LGD#01\")");
                 if(statistic != null) {
                     statisticOption.displaySpecificOne(statistic);
                 }
                 statisticMenu();
             }
             case 2 -> {
-                String statistic = JOptionPane.showInputDialog("Enter player id or last name");
+                String statistic = JOptionPane.showInputDialog("Enter player ID or last name");
                 if(statistic != null) {
                     statisticOption.displaySpecificPlayerStatistics(statistic);
                 }
                 statisticMenu();
             }
             case 3 -> {
-                String statistic = JOptionPane.showInputDialog("Enter match id (for example: \"LEG-WIS\"");
+                String statistic = JOptionPane.showInputDialog("Enter match ID (for example: \"LEG-WIS\"");
                 if(statistic != null) {
                     statisticOption.displaySpecificMatchStatistics(statistic);
                 }
